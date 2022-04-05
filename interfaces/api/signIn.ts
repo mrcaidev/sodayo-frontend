404832c1dc0baca380bdf91636de662b4a27d711
mrcaidev <mrcaidev@qq.com>
2022-04-05 14:23:58 +0800

    :sparkles: Sign in api

---
+++ interfaces/api/signIn.ts
@@ -0,0 +1,10 @@
+import { CommonResponse } from "./common";
+
+export interface SignInPayload {
+  account: string;
+  password: string;
+}
+
+export interface SignInResponse extends CommonResponse {
+  token?: string;
+}
