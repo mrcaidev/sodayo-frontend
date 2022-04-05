d5532e61da6218d590042b73b66bc6d54ca02cb6
mrcaidev <mrcaidev@qq.com>
2022-04-05 14:20:04 +0800

    :sparkles: Sign up api

---
+++ interfaces/api/signUp.ts
@@ -0,0 +1,10 @@
+import { CommonResponse } from "./common";
+
+export interface SignUpPayload {
+  phone: string;
+  password: string;
+}
+
+export interface SignUpResponse extends CommonResponse {
+  token?: string;
+}
---
+++ pages/api/user/sign-up.ts
@@ -0,0 +1,32 @@
+import { SignUpPayload, SignUpResponse } from "interfaces/api/signUp";
+import { NextApiRequest, NextApiResponse } from "next";
+import { UserService } from "services/user";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<SignUpResponse>
+) {
+  // Allow POST only.
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ error: "不允许的请求方法" });
+    return;
+  }
+
+  // Ensure phone and password exist.
+  const { phone, password } = req.body as SignUpPayload;
+  if (!phone || !password) {
+    res.status(401).json({ error: "数据缺失" });
+    return;
+  }
+
+  // Sign up.
+  try {
+    const token = await UserService.signUp(phone, password);
+    res.status(200).json({ token });
+    return;
+  } catch (e) {
+    res.status(401).json({ error: String(e) });
+    return;
+  }
+}
