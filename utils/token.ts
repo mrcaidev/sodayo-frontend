422bc25cc44da32e0472af847efdeea7f514ec22
mrcaidev <mrcaidev@qq.com>
2022-04-05 17:19:30 +0800

    :fire: Remove JWT validation

--- utils/token.ts
+++ utils/token.ts
@@ -1,5 +1,4 @@
 import { sign, TokenExpiredError, verify } from "jsonwebtoken";
-import isJWT from "./validators/isJWT";
 
 const jwtSecret = process.env.JWT_SECRET as string;
 const expiresIn = 60 * 60 * 20;
@@ -9,11 +8,6 @@ export function generateToken(userId: string) {
 }
 
 export function decodeToken(token: string) {
-  // Validate token.
-  if (!isJWT(token)) {
-    throw new Error("不合法的token");
-  }
-
   // Decode.
   try {
     const { userId } = verify(token, jwtSecret) as { userId: string };
