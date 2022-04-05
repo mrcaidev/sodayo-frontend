657f7ca5ae7fae7ebbf9dedd522a3169b8827508
mrcaidev <mrcaidev@qq.com>
2022-04-05 14:03:55 +0800

    :lock: Token validation

--- utils/token.ts
+++ utils/token.ts
@@ -1,4 +1,5 @@
-import { sign, verify } from "jsonwebtoken";
+import { sign, TokenExpiredError, verify } from "jsonwebtoken";
+import isJWT from "./validators/isJWT";
 
 const jwtSecret = process.env.JWT_SECRET as string;
 const expiresIn = 60 * 60 * 20;
@@ -8,11 +9,21 @@ export function generateToken(userId: string) {
 }
 
 export function decodeToken(token: string) {
+  // Validate token.
+  if (!isJWT(token)) {
+    throw new Error("不合法的token");
+  }
+
+  // Decode.
   try {
     const { userId } = verify(token, jwtSecret) as { userId: string };
     return userId;
   } catch (e) {
-    console.error(e);
-    return "";
+    // If token has expired.
+    if (e instanceof TokenExpiredError) {
+      throw new Error("Token已失效");
+    }
+    // Otherwise.
+    throw e;
   }
 }
