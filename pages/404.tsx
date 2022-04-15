bf1a383dd4d0893f8742c5a7e68bf44e4e588154
mrcaidev <mrcaidev@qq.com>
2022-04-15 15:34:27 +0800

    :lipstick: 404 page vertically center

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,11 @@
+import Container from "@mui/material/Container";
 import Image from "next/image";
 import notFoundSvg from "public/assets/images/404.svg";
 
 export default function NotFoundPage() {
-  return <Image src={notFoundSvg} alt="" priority />;
+  return (
+    <Container maxWidth="md" sx={{ my: "auto" }}>
+      <Image src={notFoundSvg} alt="" priority />
+    </Container>
+  );
 }
